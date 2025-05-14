c2808c4c81bd4fe313c117804cc4a7d55fadcfae
Berat Ölmez <159007239+seberatolmez@users.noreply.github.com>
2025-05-14 14:46:21 +0300

    feat: add clean arch layers

---
+++ src/main/java/com/bootcampcqrs/example/BootcampProject_CQRS/BootcampProjectCqrsApplication.java
@@ -0,0 +1,13 @@
+package com.bootcampcqrs.example.BootcampProject_CQRS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampProjectCqrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampProjectCqrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcampcqrs/example/BootcampProject_CQRS/BootcampProjectCqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcampcqrs.example.BootcampProject_CQRS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampProjectCqrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
